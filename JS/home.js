3424f17b9f9fd574b99c4992d3bb62f5427f4f69
Garrett Goehring <gmgoehring@gmail.com>
2015-05-02 16:03:45 -0500

    added 6 sections w/ possible types

--- JS/home.js
+++ JS/home.js
@@ -1,8 +1,29 @@
 $(window).scroll(function () {
     winHeight = $(window).height();
     if ($(window).scrollTop() > winHeight) {
-        $(".navbar").css("background-color","white");
-        $('.navbar').attr("class","navbar-fixed-top");
-        $("#section2").css("top","-3.2%") 
+        $(".navbar").css("background-color","black");
+        $(".navbar").css("color","white");
+        $('.navbar').attr("class","navbar-fixed-top ");
+        $("#section2").css("top","-6.35%") 
     }
 });
+
+$(".fact-buble").hover(
+	function(){
+		$(this).css("height","250px");
+		$(this).css("width","250px");
+		$(this).css("background-color","black");
+		$(this).css("color","white");
+		$(this).css("font-size","1.3em");
+		$(this).css("padding-top","2.5%");
+
+	},
+	function(){
+		$(this).css("height","200px");
+		$(this).css("width","200px");
+		$(this).css("background-color","transparent");
+		$(this).css("color","black");
+		$(this).css("font-size","1em");
+		$(this).css("padding-top","1.5%");
+	}
+)
